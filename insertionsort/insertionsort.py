66389ca77b131ca354898fe6c10868cdabda7bc5
szenadam <szenadam1@gmail.com>
2018-09-23 10:17:32 +0100

    Add python insertion sort

---
+++ insertionsort/insertionsort.py
@@ -0,0 +1,10 @@
+array = [1,4,8,21,6,1,12,5]
+
+def insertion_sort(collection):
+    for i in range(1, len(collection)):
+        while i > 0 and collection[i-1] > collection[i]:
+            collection[i], collection[i-1] = collection[i-1], collection[i]
+            i -= 1
+    return collection
+
+print(insertion_sort(array))
