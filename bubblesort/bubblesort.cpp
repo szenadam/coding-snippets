98977d7894b273acc15bbb7ec14c469b551c6577
szenadam <szenadam1@gmail.com>
2018-09-23 10:15:27 +0100

    Fix warns, add printVector to cpp bubblesort

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -1,18 +1,22 @@
+#include <iostream>
 #include <stdio.h>
 #include <vector>
 
 void bubblesort(std::vector<int>& a);
+void printVector(std::vector<int> const &input);
 
 int main() {
     std::vector<int> a {1,4,2,7,1,45,1,7,2,2};
+    bubblesort(a);
+    printVector(a);
     return 0;
 }
 
 void bubblesort(std::vector<int>& a) {
-    for(int i = 0; i < a.size(); i++)
+    for(unsigned int i = 0; i < a.size(); i++)
     {
         bool swapped = false;
-        for(int j = 0; j < a.size() - i -1; j++)
+        for(unsigned int j = 0; j < a.size() - i -1; j++)
         {
             if (a[j] > a[j+1]) {
                 swapped = true;
@@ -23,4 +27,11 @@ void bubblesort(std::vector<int>& a) {
         }
         if (!swapped) break;
     }
+}
+
+void printVector(std::vector<int> const &input)
+{
+	for (int i = 0; i < input.size(); i++) {
+		std::cout << input.at(i) << ' ';
+	}
 }
