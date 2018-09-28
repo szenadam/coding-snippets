184b1abe299ebce16b669065a22ee451dd1872c6
szenadam <szenadam1@gmail.com>
2018-09-28 09:21:55 +0100

    Move sorting snippets to own folder

---
+++ sorting-snippets/quicksort/quicksort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void quicksort(int array[]) {
+    int length = sizeof(array)/sizeof(*array);
+
+    if (length <= 1) return;
+
+    int pivot = array[0];
+
+
+}
+
+int main() {
+
+    return 0;
+}
